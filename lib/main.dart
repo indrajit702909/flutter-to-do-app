8dbc1fa3a14de80e51ee14677affa35a598b6315
indrajit702909 <indrajitdasofficial.wb@gmail.com>
2024-01-22 22:33:26 +0530

    Firebase error fixed

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ import 'package:to_do/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
